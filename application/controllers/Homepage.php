85ebd409f4b7a5fa5abdd0dc79e6de1a2a82a73c
Jason Kolenosky <JasonK58@gmail.com>
2016-12-11 17:02:35 -0800

    Added recipes created and earnings summary to homepage view.
    Signed-off-by: Jason Kolenosky <JasonK58@gmail.com>

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -1,43 +1,74 @@
 <?php
-    defined('BASEPATH') OR exit('No direct script access allowed');
 
-    class Homepage extends Application
-    {
-        public function __construct() {
-	parent::__construct();
-            $this->load->model('LogModel');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-	}
-        
+class Homepage extends Application {
 
-        /**
-         * Index Page for this controller.
-         *
-         * Maps to the following URL
-         *        http://example.com/index.php/welcome
-         *    - or -
-         *        http://example.com/index.php/welcome/index
-         *    - or -
-         * Since this controller is set as the default controller in
-         * config/routes.php, it's displayed at http://example.com/
-         *
-         * So any other public methods not prefixed with an underscore will
-         * map to /index.php/welcome/<method_name>
-         * @see https://codeigniter.com/user_guide/general/urls.html
-         */
-        public function index()
-        {
-            $this->data['pagebody'] = 'homepage_view';
-            $this->data['pagetitle'] = 'Homepage';
-            
-            $moneySpent = $this->LogModel->RetrieveMoneySpentPurchasing();
-            $moneyEarned = $this->LogModel->RetrieveMoneyEarnedSales();
-            $costOfConsumed = $this->LogModel->RetrieveCostofConsumed();
-            
-            $this->data['moneySpent'] = $moneySpent;
-            $this->data['moneyEarned'] = $moneyEarned;
-            $this->data['consumedCost'] = $costOfConsumed;
-
-            $this->render();
-        }
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('LogModel');
     }
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     *        http://example.com/index.php/welcome
+     *    - or -
+     *        http://example.com/index.php/welcome/index
+     *    - or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index() {
+        $this->data['pagebody'] = 'homepage_view';
+        $this->data['pagetitle'] = 'Homepage';
+        
+        $this->summarize();
+
+        $this->render();
+    }
+
+public function summarize() {
+        $this->load->helper('directory');
+        $candidates = directory_map('../data/');
+        
+        $mapOfRecipeCounts = array();
+        $mapOfRecipesSold = array();
+        
+        foreach ($candidates as $filename) {
+            if (substr($filename, 0, 6) == 'recipe') {
+                $xml = simplexml_load_file('../data/' . $filename);
+                $recipeName = (string) $xml->recipeName;
+                
+                if (!array_key_exists($recipeName, $mapOfRecipeCounts)) {
+                    $mapOfRecipeCounts[$recipeName] = 1;
+                } else {
+                    $mapOfRecipeCounts[$recipeName] += 1;
+                }
+            }
+        }
+        
+        foreach ($candidates as $filename) {
+            if (substr($filename, 0, 5) == 'stock') {
+                $xml = simplexml_load_file('../data/' . $filename);
+                $itemName = (string) $xml->itemName;
+                $sellingPrice = (string) $xml->sellingPrice;
+                
+                if (!array_key_exists($itemName, $mapOfRecipesSold)) {
+                    $mapOfRecipesSold[$itemName] = $sellingPrice;
+                } else {
+                    $mapOfRecipesSold[$itemName] += $sellingPrice;
+                }
+            }
+        }
+        
+        $this->data['mapOfRecipeCounts'] = $mapOfRecipeCounts;
+        $this->data['mapOfRecipesSold'] = $mapOfRecipesSold;
+}
+
+}
--- application/views/homepage_view.php
+++ application/views/homepage_view.php
@@ -7,17 +7,36 @@
 <table class="table" style="width:500px">
   <thead>
     <tr>
-      <th>Money Spent</th>
-      <th>Money Earned</th>
-      <th>Consuming Cost</th>
+      <th>Recipe Name</th>
+      <th>Amount Created</th>
     </tr>
   </thead>
   <tbody>
+<?php foreach($this->data['mapOfRecipeCounts'] as $name => $amount): ?>
+  <tr>
+     <td><?php echo $name ?></td>
+     <td><?php echo $amount ?></td>
+  </tr>
+<?php endforeach; ?>
+  </tbody>
+</table>
+</div>
+
+<div style="width:80%; margin:auto; margin-top:1em">
+<table class="table" style="width:500px">
+  <thead>
     <tr>
-      <td>{moneySpent}</td>
-      <td>{moneyEarned}</td>
-      <td>{consumedCost}</td>
+      <th>Recipe Name</th>
+      <th>Total Value Sold</th>
     </tr>
+  </thead>
+  <tbody>
+<?php foreach($this->data['mapOfRecipesSold'] as $first_name => $last_name): ?>
+  <tr>
+     <td><?php echo $first_name ?></td>
+     <td><?php echo $last_name ?></td>
+  </tr>
+<?php endforeach; ?>
   </tbody>
 </table>
 </div>
